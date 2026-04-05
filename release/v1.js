8487c8883ee7f996552e80960029c1b5bd474464
asterf <ists821@gmail.com>
2026-04-05 15:43:45 +0900

    update v1

--- release/v1.js
+++ release/v1.js
@@ -1,6 +1,8 @@
 globalThis.d=document;
 globalThis.$=q=>d.querySelector(q);
+globalThis.$$=q=>d.querySelectorAll(q);
 globalThis.HTMLElement.prototype.$=function(q){return this.querySelector(q)};
+globalThis.HTMLElement.prototype.$$=function(q){return this.querySelectorAll(q)};
 globalThis.HTMLElement.prototype.rm=function(){return this.remove()};
 globalThis.parseHTML=function(html){
   const len=html.length,VOIDS={area:1,base:1,br:1,col:1,embed:1,hr:1,img:1,input:1,link:1,meta:1,param:1,source:1,track:1,wbr:1},ENTS={amp:'&',lt:'<',gt:'>',quot:'"',apos:"'"},fragment=d.createDocumentFragment();
